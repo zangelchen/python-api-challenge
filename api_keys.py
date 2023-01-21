8860136abbffccd403cc3fdcfce6916e13aa9242
zangelchen <ziyuechen@MacBook-Pro-2.local>
2023-01-21 15:31:54 -0700

    first commit

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "3f2299737893ae57b210a3f0c35b203a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "86da42164fb643099a83452769757230"
